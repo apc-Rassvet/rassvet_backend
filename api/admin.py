72fa8e667b02b80b839e7bc8234b1c86db259452
Andrey-0367 <shestakov0367@yandex.ru>
2025-03-31 13:54:43 +0300

    create Review

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Review
+from .forms import ReviewForm
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    form = ReviewForm
+    list_display = ('title', 'author', 'created_at', 'is_active')
+    list_filter = ('is_active', 'created_at')
+    search_fields = ('title', 'content', 'author__username')
+    readonly_fields = ('created_at', 'updated_at')
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'content', 'is_active')
+        }),
+        ('Системная информация', {
+            'fields': ('author', 'created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:  # Если это новый объект
+            obj.author = request.user
+        super().save_model(request, obj, form, change)
