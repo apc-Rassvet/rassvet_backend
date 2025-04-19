47be2125c6199a32d7e3a078ebd6576ec922368b
Михаил Миноцкий <smoke-m@yandex.com>
2025-04-19 12:12:54 +0300

    Добавленно описание ендпоинтов API

--- content/api/views.py
+++ content/api/views.py
@@ -1,13 +1,25 @@
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import viewsets, filters
 from rest_framework.pagination import LimitOffsetPagination
-from drf_spectacular.utils import extend_schema, extend_schema_view
 
 from content import models
 from content import pagination
 from . import serializers
 
 
+@extend_schema(tags=["Gratitudes group"])
+@extend_schema_view(
+    list=extend_schema(
+        summary="Получить список Благодарностей.",
+    ),
+    retrieve=extend_schema(
+        summary="Получить Благодарность по ID.",
+    )
+)
 class GratitudeViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    Получить все Благодарности списком, или конкретную по ID.
+    """
     queryset = models.Gratitude.objects.filter(is_active=True)
     serializer_class = serializers.GratitudeSerializer
     filter_backends = [filters.OrderingFilter]
@@ -16,7 +28,19 @@ class GratitudeViewSet(viewsets.ReadOnlyModelViewSet):
     pagination_class = pagination.GratitudePagination
 
 
+@extend_schema(tags=["Partners group"])
+@extend_schema_view(
+    list=extend_schema(
+        summary="Получить список карточек Сотрудников.",
+    ),
+    retrieve=extend_schema(
+        summary="Получить карточку Сотрудника по ID.",
+    )
+)
 class PartnersViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    Получить все карточки Сотрудников списком, или конкретную по ID.
+    """
     queryset = models.Partner.objects.all()
     serializer_class = serializers.PartnersSerializer
     filter_backends = [filters.OrderingFilter]
@@ -25,14 +49,38 @@ class PartnersViewSet(viewsets.ReadOnlyModelViewSet):
     pagination_class = LimitOffsetPagination
 
 
+@extend_schema(tags=["Reviews group"])
+@extend_schema_view(
+    list=extend_schema(
+        summary="Получить список Отзывов.",
+    ),
+    retrieve=extend_schema(
+        summary="Получить Отзыв по ID.",
+    )
+)
 class ReviewViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    Получить все Отзывы списком, или конкретный по его ID.
+    """
     queryset = models.Review.objects.filter(is_active=True)
     serializer_class = serializers.ReviewSerializer
     pagination_class = pagination.ReviewPagination
     ordering = ['-created_at']
 
 
+@extend_schema(tags=["Videos group"])
+@extend_schema_view(
+    list=extend_schema(
+        summary="Получить список Видео.",
+    ),
+    retrieve=extend_schema(
+        summary="Получить Видео по ID.",
+    )
+)
 class VideoViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    Получить все Видео списком, или конкретное по его ID.
+    """
     queryset = models.Video.objects.filter(is_active=True)
     serializer_class = serializers.VideoSerializer
     filter_backends = [filters.OrderingFilter]
@@ -40,19 +88,22 @@ class VideoViewSet(viewsets.ReadOnlyModelViewSet):
     ordering = ['-created_at']
 
 
-@extend_schema(tags=["Test group."])
+@extend_schema(tags=["Fundraisings group"])
 @extend_schema_view(
     list=extend_schema(
-        summary="Получить список.",
-        # description="""Описание list.""",
+        summary="Получить список Адресных сборов.",
+        description="""
+        Получить список Адресных сборов с достаточной информацией о сборе.
+        """,
     ),
     retrieve=extend_schema(
-        summary="Получить по id.",
-        # description="""Описание retrieve.""",
+        summary="Получить Адресный сбор по ID.",
+        description="""
+        Получить Адресный сбор по ID с полной информацией о сборе.
+        """
     )
 )
 class TargetedFundraisingViewSet(viewsets.ReadOnlyModelViewSet):
-    """Описание для всех list + retrieve +.. ."""
     queryset = models.TargetedFundraising.objects.all()
     ordering_fields = ['order', 'created_at']
     ordering = ['order', '-created_at']
