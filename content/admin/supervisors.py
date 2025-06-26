00bf079301b5bb644906ced48c9a3d3ffca7a4e4
Vladimir Shvab <shvab.vladimir@gmail.com>
2025-06-27 00:59:01 +0300

    #24: feat: add orderedmodel and directions to supervisor, improve supervisor admin

--- content/admin/supervisors.py
+++ content/admin/supervisors.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 
+from content.base_models import BaseOrderedModelAdmin
 from content.models.supervisors import Supervisor
 
 
 @admin.register(Supervisor)
-class PageAdmin(admin.ModelAdmin):
+class PageAdmin(BaseOrderedModelAdmin):
     """Модель администрирования супервизоров."""
 
-    list_display = ('name', 'page')
-    list_filter = ('page',)
+    list_display = ('name', 'position', 'move_up_down_links')
+    readonly_fields = ('created_at', 'updated_at')
+    list_filter = ('directions',)
+    filter_horizontal = ('directions',)
--- content/models/supervisors.py
+++ content/models/supervisors.py
@@ -1,32 +1,34 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
+from ordered_model.models import OrderedModel
 
-class Supervisor(models.Model):
+from content.constants import IMAGE_CONTENT_TYPES
+from content.mixins import TimestampMixin
+
+from . import Direction
+
+
+class Supervisor(TimestampMixin, OrderedModel):
     """Модель для хранения информации о Супервизоров центра."""
 
     name = models.CharField(max_length=255, verbose_name='ФИО')
-    image = models.ImageField(upload_to='supervisors/', verbose_name='Фото')
+    image = models.ImageField(
+        upload_to='supervisors/',
+        verbose_name='Фото',
+        validators=[FileExtensionValidator(IMAGE_CONTENT_TYPES)],
+    )
     position = models.CharField(max_length=255, verbose_name='Должность')
-    ordering = models.PositiveSmallIntegerField(
-        default=0, verbose_name='Порядок'
-    )
-    PAGE_CHOICES = (
-        ('aba-therapy', 'ABA-терапия'),
-        ('adaptive-physical-culture', 'Адаптивная физкультура'),
-        ('creative-workshops', 'Творческие мастерские'),
-        ('resource-classes', 'Ресурсные классы'),
-        ('children-leisure', 'Досуг для детей'),
-    )
-    page = models.SlugField(
-        max_length=255,
-        choices=PAGE_CHOICES,
-        verbose_name='Направление деятельности',
+    directions = models.ManyToManyField(
+        Direction, verbose_name='Направление деятельности'
     )
 
     class Meta:
         verbose_name = 'Супервизор'
         verbose_name_plural = 'Супервизоры'
-        ordering = ['ordering']
+        ordering = [
+            'order',
+        ]
 
     def __str__(self):
         """Возвращает строковое представление супервизора."""
