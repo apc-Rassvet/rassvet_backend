7e0f716ec6156564cd5bf0ec1bed34aee1d619aa
Vladimir Shvab <shvab.vladimir@gmail.com>
2025-07-06 22:54:21 +0300

    #51: feat: add literature api with pagination

--- content/pagination.py
+++ content/pagination.py
@@ -1,7 +1,37 @@
-from rest_framework.pagination import LimitOffsetPagination
+from collections import OrderedDict
+
+from rest_framework.pagination import (
+    LimitOffsetPagination,
+    PageNumberPagination,
+)
+from rest_framework.response import Response
 
 
 class NewsLimitOffsetPagination(LimitOffsetPagination):
     """Пагинация для новостей."""
 
     default_limit = 6
+
+
+class LiteraturePageNumberPagination(PageNumberPagination):
+    """Пагинация для литературы."""
+
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        """Генерирует ответ с пагинацией и дополнительными полями."""
+        return Response(
+            OrderedDict(
+                [
+                    ('count', self.page.paginator.count),
+                    ('total_pages', self.page.paginator.num_pages),
+                    ('current_page', self.page.number),
+                    ('page_size', self.get_page_size(self.request)),
+                    ('next', self.get_next_link()),
+                    ('previous', self.get_previous_link()),
+                    ('results', data),
+                ]
+            )
+        )
