84f57c83a6dd9362c675a81cc224114c8e710fd0
Vladimir Shvab <shvab.vladimir@gmail.com>
2025-07-12 15:59:11 +0300

    style: del commented debug=true from settings

--- rassvet/settings.py
+++ rassvet/settings.py
@@ -19,7 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'default-key')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-# DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
