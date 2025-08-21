90c850d4f5befbc31023a09c3f84a0265c53b574
Vladimir Shvab <shvab.vladimir@gmail.com>
2025-08-21 10:03:30 +0300

    #94:feat: integrate CharCountAdminMixin into SupervisorAdmin for character count validation

--- content/admin/supervisors.py
+++ content/admin/supervisors.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 
 from content.base_models import BaseOrderedModelAdmin
+from content.mixins import CharCountAdminMixin
 from content.models.supervisors import Supervisor
 
 
 @admin.register(Supervisor)
-class SupervisorAdmin(BaseOrderedModelAdmin):
+class SupervisorAdmin(CharCountAdminMixin, BaseOrderedModelAdmin):
     """Модель администрирования супервизоров."""
 
+    charcount_fields = {
+        'name': 19,
+        'position': 70,
+    }
     list_display = ('name', 'position', 'move_up_down_links')
     readonly_fields = ('created_at', 'updated_at')
     list_filter = (('directions', admin.RelatedOnlyFieldListFilter),)
